b0b334cdd98228d60a9e47cd1f7d8007dde88e9a
raihan-brain <raihanuzzaman@bs-23.net>
2022-12-14 15:16:03 +0600

    added dependencies cache 4

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -9,6 +9,7 @@ function MainContent() {
     setHelpVisible((isVisible) => !isVisible);
   }
 
+  // added hashFiles function in github workflow
   return (
     <main>
       <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
